a19d39d0e4d86495e551e1a9056727743c91b4b2
Emilka <emilka031916@gmail.com>
2024-11-06 08:42:52 +0100

    ponowna zmiana foty

--- src/components/HeaderTop.tsx
+++ src/components/HeaderTop.tsx
@@ -8,28 +8,8 @@ const HeaderTop: React.FC = () => {
           <div className="row">
             <div className="col-lg-6 col-md-12 col-sm-12">
               <div className="contact-info">
-                <i className="las la-envelope"></i> info@industrify.com |
-                <i className="las la-map-marker"></i> 3567 Melbourn, EA 265,
-                Australia
-              </div>
-            </div>
-            <div className="col-lg-6 col-md-12 col-sm-12 text-end">
-              <div className="site-info">
-                Turning big ideas into great products!
-                <div className="social-area">
-                  <a href="">
-                    <i className="lab la-facebook-f"></i>
-                  </a>
-                  <a href="">
-                    <i className="lab la-instagram"></i>
-                  </a>
-                  <a href="">
-                    <i className="lab la-twitter"></i>
-                  </a>
-                  <a href="">
-                    <i className="la la-skype"></i>
-                  </a>
-                </div>
+                <i className="las la-envelope"></i> mail@parol.com |
+                <i className="las la-map-marker"></i> aleja Legionów 19, 08-400 Garwolin
               </div>
             </div>
           </div>
